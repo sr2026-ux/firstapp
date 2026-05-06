c27bbc9d6fa6837b186a6fafb2c9b85bcbee2a50
sr2026-ux <sanjuraj@redhat.com>
2026-05-06 09:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcometolws():
-    return "Welcome to LWS!"
+    return "Welcome to Learn With Sanju!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
